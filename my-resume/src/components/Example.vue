eb71c9821722dce783d4ef896040d21a1628baa6
helloberries <helloberriesqz@gmail.com>
2016-06-15 10:04:45 +0800

    start

---
+++ my-resume/src/components/Example.vue
@@ -0,0 +1,22 @@
+<template>
+	<button v-on:click="greet" class="btn">Greet {{ name }}</button>
+</template>
+
+<script>
+	export default {
+		name: 'Example',
+		data: function () {
+			return {
+				name: 'Vue.js'
+			}
+		},
+		methods: {
+	    greet: function (event) {
+	      // 方法内 `this` 指向 vm
+	      alert('Hello ' + this.name + '!')
+	      // `event` 是原生 DOM 事件
+	      alert(event.target.tagName)
+    }
+  }
+	}
+</script>
