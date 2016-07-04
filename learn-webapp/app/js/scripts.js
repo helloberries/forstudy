8202385a044abeefad8efb1e1a0774cee876ce9c
helloberries <helloberriesqz@gmail.com>
2016-07-04 17:08:30 +0800

    make 2 views

--- learn-webapp/app/js/scripts.js
+++ learn-webapp/app/js/scripts.js
@@ -1,8 +1,11 @@
 window.onload = function () {
-  let todo-categories = document.getElementsByClassName('todo-category');
-  for (var i = 0; i<todo-categories.length; i++) {
-    todo-categories.addEventListener('click',function () {
-      
+  var todoCategories = document.getElementsByClassName('todo-category');
+  var views = document.getElementsByClassName('view');
+  for (var i = 0; i<todoCategories.length; i++) {
+    todoCategories[i].addEventListener('click',function () {
+      views[0].className = 'view';
+      document.getElementById('todo1').className = 'view current';
+      console.log('hi');
     }, false);
   }
 };
