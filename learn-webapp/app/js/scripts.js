a157cd1b56533d109473b2a25b7dd56acdb5fa65
helloberries <helloberriesqz@gmail.com>
2016-07-04 16:54:40 +0800

    add scripts

---
+++ learn-webapp/app/js/scripts.js
@@ -0,0 +1,8 @@
+window.onload = function () {
+  let todo-categories = document.getElementsByClassName('todo-category');
+  for (var i = 0; i<todo-categories.length; i++) {
+    todo-categories.addEventListener('click',function () {
+      
+    }, false);
+  }
+};
